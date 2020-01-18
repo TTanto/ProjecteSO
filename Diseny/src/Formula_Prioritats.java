341b8b79048d634c01fa2bbb45019cd76700fd4b
Mane9mb <mmoreno9mb@gmail.com>
2020-01-18 18:31:56 +0100

    Re commit
    Creacio de finestres amb les formules per cada algoritme.
    
    Colaboradors: Manel,Oriol Vivó,Antoni carol,Oriol figueres

---
+++ Diseny/src/Formula_Prioritats.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Formula_Prioritats {
+    private JLabel Tresposta_Mitj;
+    private JLabel TretornMitj;
+    private JLabel TesperaMitj;
+    private JLabel sol_TesperaM;
+    private JLabel TretornM;
+    private JLabel sol_TretorN_Mitj;
+}
---
+++ Diseny/src/Formulas_SJF.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Formulas_SJF {
+    private JLabel TretornN_Mitj;
+    private JLabel TretornMitj;
+    private JLabel TesperaMitj;
+    private JLabel sol_TesperaM;
+    private JLabel TretornM;
+    private JLabel sol_TretorN_Mitj;
+}
